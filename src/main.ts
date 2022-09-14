6a8b0d65ff506bd57a4bb78bdbf9b3f288e1a755
Cody Clark <opuseleven@protonmail.com>
2022-09-13 21:02:18 -0500

    Fixed flattenFeature & unFlattenFeature functions

--- src/main.ts
+++ src/main.ts
@@ -55,7 +55,7 @@ const sampleFeatures: FeatureCollection = {
   ],
 };
 
-type FlattenedFeature = Omit<Feature, 'geometry'> & { geometry: any };
+type FlattenedFeature = Omit<Feature, 'geometry'> & { geometry: String[] };
 
 /**
  * Take a standard GeoJSON structure in, return a Featurelike object whose geometry property is more flat (less deeply nested)
@@ -63,25 +63,28 @@ type FlattenedFeature = Omit<Feature, 'geometry'> & { geometry: any };
 function flattenFeature(input: Feature): FlattenedFeature {
   const featureType = input.geometry.type;
 
-  let returnArr = [featureType];
+  let returnArr: String[] = [featureType];
+
+  function flattenPoint(point: Number[]): String {
+    let coordString = '';
+    for (let i = 0; i < point.length; i++) {
+      coordString = [coordString, point[i]].join(', ');
+    }
+    return coordString.substring(2);
+  }
 
   if (input.geometry.type === 'Point') {
-    for (let i = 0; i < input.geometry.coordinates.length; i++) {
-      returnArr.push(input.geometry.coordinates[i].toString();
-    }
+    returnArr.push(flattenPoint(input.geometry.coordinates));
   } else if (input.geometry.type === 'LineString') {
     for (let i = 0; i < input.geometry.coordinates.length; i++) {
-      for (let j = 0; j < input.geometry.coordinates[i].length; j++) {
-        returnArr.push(input.geometry.coordinates[i][j].toString());
-      }
+      returnArr.push(flattenPoint(input.geometry.coordinates[i]));
     }
   } else if (input.geometry.type === 'Polygon') {
     for (let i = 0; i < input.geometry.coordinates.length; i++) {
       for (let j = 0; j < input.geometry.coordinates[i].length; j++) {
-        for (let k = 0; k < input.geometry.coordinates[i][j].length; k++) {
-          returnArr.push(input.geometry.coordinates[i][j][k].toString());
-        }
+        returnArr.push(flattenPoint(input.geometry.coordinates[i][j]));
       }
+      returnArr.push('||'); // esc char to designate start of new array
     }
   }
 
@@ -98,7 +101,50 @@ function flattenFeature(input: Feature): FlattenedFeature {
  * Inverse operation of 'flattenFeature'
  */
 function unFlattenFeature(input: FlattenedFeature): Feature {
-  return input;
+  const featureType = input.geometry[0];
+
+  let coordinateArr: any[] = [];
+  let polygonArr: any[] = [];
+
+  function unFlattenPoint(point: String) {
+    const stringArr = point.split(', ');
+    let numArr = [];
+    for (let i = 0; i < stringArr.length; i++) {
+      numArr.push(parseFloat(stringArr[i]))
+    }
+    return numArr;
+  }
+
+  if (featureType === 'Point') {
+    const pointArr = unFlattenPoint(input.geometry[1]);
+    coordinateArr = [...pointArr];
+  } else if (featureType === 'LineString') {
+    for (let i = 1; i < input.geometry.length; i++) {
+      const pointArr = unFlattenPoint(input.geometry[i]);
+      coordinateArr.push(pointArr);
+    }
+  } else if (featureType === 'Polygon') {
+    for (let i = 1; i < input.geometry.length; i++) {
+      if (input.geometry[i] === '||') {
+        polygonArr.push(coordinateArr);
+        coordinateArr = [];
+      } else {
+        const pointArr = unFlattenPoint(input.geometry[i]);
+        coordinateArr.push(pointArr);
+      }
+    }
+  }
+
+  const returnFeature = {
+    type: input.type,
+    geometry: {
+      type: featureType,
+      coordinates: featureType === 'Polygon' ? polygonArr : coordinateArr,
+    },
+    properties: input.properties
+  }
+  ''
+  return returnFeature;
 }
 
 function main() {
